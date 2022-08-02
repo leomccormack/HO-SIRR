83be97e62206689f82be76fb49f44d4f37bd009d
Chris Hold <Christoph.Hold@aalto.fi>
2022-08-02 16:36:54 +0300

    Update HOSIRR_bin_TEST.m

--- experimental/HOSIRR_bin_TEST.m
+++ experimental/HOSIRR_bin_TEST.m
@@ -4,70 +4,70 @@ clear all, close all %, dbstop if error %#ok
 addpath '..'
 addpath '../_Simulated_Rooms_' '../_Stimuli_'
 
-demo_order = 3;  
+demo_order = 8;  
 [input_stimulus, fs_in] = audioread('music__KickDrumClicky.wav');
 sofapath = '~/data/HRTFs/Kemar_Aalto_2016/KemarAuralID.sofa';
 
-%% FIRST-ORDER SIRR BIN CHECKS
-pars.order = 1;  
-pars.fs = 48e3; 
-[~,dirs_rad] = getTdesign(2*(pars.order+1));
-pars.ls_dirs_deg = dirs_rad*180/pi;
-pars.multires_winsize = 128;  
-pars.multires_xovers = [];   
-pars.RENDER_DIFFUSE = 1;
-pars.BROADBAND_FIRST_PEAK = 0; %%%DISABLED     
-pars.nBroadbandPeaks = 1;    
-pars.decorrelationType = 'phase';  %%%PHASE
-pars.BROADBAND_DIFFUSENESS = 0;
-pars.maxDiffFreq_Hz = 3000;  
-pars.alpha_diff = 0.5;
-pars.chOrdering = 'ACN';
-pars.normScheme = 'N3D';
-pars.hrtf_sofa_path = sofapath;
-
-%% --- Single plane-wave input --- 
-src_dir = [-45 -45];  % try adding more than 1 plane-wave, to see the first-order analysis break
-shir = randn(pars.fs, size(src_dir,1)) * (sqrt(4*pi).*getRSH(pars.order, src_dir)).';
-[sirr,~,~,pars,analysis] = HOSIRR_bin(shir, pars);
-
-% In this case, we would expect:
-% - [azimuth elevation] should correspond to 'src_dir'  
-% - diffuseness should be 0
-% - non-diffuse energy should be similar to input sound-field energy, and 
-%   diffuse energy ~0
-%figure, subplot(4,1,1), imagesc(analysis.azim{1}*180/pi), colorbar, axis xy, caxis([-180 180]), title('azimuth (degrees)')
-%subplot(4,1,2), imagesc(analysis.elev{1}*180/pi), colorbar, axis xy, caxis([-90 90]), title('elevation (degrees)')
-%subplot(4,1,3), imagesc(10*log10(analysis.energy{1})), colorbar, axis xy, title('energy (dB)')
-%subplot(4,1,4), imagesc(analysis.diff{1}), colorbar, axis xy, caxis([0 1]), title('diffuseness')
-figure, plot(10*log10(analysis.sf_energy{1})), hold on 
-plot(10*log10(analysis.ndiff_energy{1})), hold on
-plot(10*log10(analysis.diff_energy{1})) 
-title('PW: energy (dB)'), grid on
-legend('sound-field', 'non-diffuse', 'diffuse')
-%figure, plot(analysis.diff{1}(1,:)), title('diffuseness'), grid on, ylim([0 1])
-
-%% --- Diffuse input --- 
-%[~, diff_dirs] = getTdesign(21); % approximate diffuse-field with 240 incoherent noise sources
-%shir = randn(pars.fs, size(diff_dirs,1)) * (sqrt(4*pi).*getRSH(pars.order, diff_dirs*180/pi)).';
-shir = randn(pars.fs, (pars.order+1)^2);
-[sirr,~,~,pars,analysis] = HOSIRR_bin(shir, pars);
-
-% In this case, we would expect:
-% - [azimuth elevation] should be random 
-% - diffuseness should be close to 1
-% - diffuse energy should be similar to the input sound-field energy, and
-%   non-diffuse energy much lower than diffuse energy
-%figure, subplot(4,1,1), imagesc(analysis.azim{1}*180/pi), colorbar, axis xy, caxis([-180 180]), title('azimuth (degrees)')
-%subplot(4,1,2), imagesc(analysis.elev{1}*180/pi), colorbar, axis xy, caxis([-90 90]), title('elevation (degrees)')
-%subplot(4,1,3), imagesc(10*log10(analysis.energy{1})), colorbar, axis xy, title('energy (dB)')
-%subplot(4,1,4), imagesc(analysis.diff{1}), colorbar, axis xy, caxis([0 1]), title('diffuseness')
-figure, plot(10*log10(analysis.sf_energy{1})), hold on 
-plot(10*log10(analysis.ndiff_energy{1})), hold on
-plot(10*log10(analysis.diff_energy{1})) 
-title('Diffuse: energy (dB)'), grid on
-legend('sound-field', 'non-diffuse', 'diffuse')
-%figure, plot(analysis.diff{1}(1,:)), title('diffuseness'), grid on, ylim([0 1])
+% %% FIRST-ORDER SIRR BIN CHECKS
+% pars.order = 1;  
+% pars.fs = 48e3; 
+% [~,dirs_rad] = getTdesign(2*(pars.order+1));
+% pars.ls_dirs_deg = dirs_rad*180/pi;
+% pars.multires_winsize = 128;  
+% pars.multires_xovers = [];   
+% pars.RENDER_DIFFUSE = 1;
+% pars.BROADBAND_FIRST_PEAK = 0; %%%DISABLED     
+% pars.nBroadbandPeaks = 1;    
+% pars.decorrelationType = 'phase';  %%%PHASE
+% pars.BROADBAND_DIFFUSENESS = 0;
+% pars.maxDiffFreq_Hz = 3000;  
+% pars.alpha_diff = 0.5;
+% pars.chOrdering = 'ACN';
+% pars.normScheme = 'N3D';
+% pars.hrtf_sofa_path = sofapath;
+% 
+% %% --- Single plane-wave input --- 
+% src_dir = [-45 -45];  % try adding more than 1 plane-wave, to see the first-order analysis break
+% shir = randn(pars.fs, size(src_dir,1)) * (sqrt(4*pi).*getRSH(pars.order, src_dir)).';
+% [sirr,~,~,pars,analysis] = HOSIRR_bin(shir, pars);
+% 
+% % In this case, we would expect:
+% % - [azimuth elevation] should correspond to 'src_dir'  
+% % - diffuseness should be 0
+% % - non-diffuse energy should be similar to input sound-field energy, and 
+% %   diffuse energy ~0
+% %figure, subplot(4,1,1), imagesc(analysis.azim{1}*180/pi), colorbar, axis xy, caxis([-180 180]), title('azimuth (degrees)')
+% %subplot(4,1,2), imagesc(analysis.elev{1}*180/pi), colorbar, axis xy, caxis([-90 90]), title('elevation (degrees)')
+% %subplot(4,1,3), imagesc(10*log10(analysis.energy{1})), colorbar, axis xy, title('energy (dB)')
+% %subplot(4,1,4), imagesc(analysis.diff{1}), colorbar, axis xy, caxis([0 1]), title('diffuseness')
+% figure, plot(10*log10(analysis.sf_energy{1})), hold on 
+% plot(10*log10(analysis.ndiff_energy{1})), hold on
+% plot(10*log10(analysis.diff_energy{1})) 
+% title('PW: energy (dB)'), grid on
+% legend('sound-field', 'non-diffuse', 'diffuse')
+% %figure, plot(analysis.diff{1}(1,:)), title('diffuseness'), grid on, ylim([0 1])
+% 
+% %% --- Diffuse input --- 
+% %[~, diff_dirs] = getTdesign(21); % approximate diffuse-field with 240 incoherent noise sources
+% %shir = randn(pars.fs, size(diff_dirs,1)) * (sqrt(4*pi).*getRSH(pars.order, diff_dirs*180/pi)).';
+% shir = randn(pars.fs, (pars.order+1)^2);
+% [sirr,~,~,pars,analysis] = HOSIRR_bin(shir, pars);
+% 
+% % In this case, we would expect:
+% % - [azimuth elevation] should be random 
+% % - diffuseness should be close to 1
+% % - diffuse energy should be similar to the input sound-field energy, and
+% %   non-diffuse energy much lower than diffuse energy
+% %figure, subplot(4,1,1), imagesc(analysis.azim{1}*180/pi), colorbar, axis xy, caxis([-180 180]), title('azimuth (degrees)')
+% %subplot(4,1,2), imagesc(analysis.elev{1}*180/pi), colorbar, axis xy, caxis([-90 90]), title('elevation (degrees)')
+% %subplot(4,1,3), imagesc(10*log10(analysis.energy{1})), colorbar, axis xy, title('energy (dB)')
+% %subplot(4,1,4), imagesc(analysis.diff{1}), colorbar, axis xy, caxis([0 1]), title('diffuseness')
+% figure, plot(10*log10(analysis.sf_energy{1})), hold on 
+% plot(10*log10(analysis.ndiff_energy{1})), hold on
+% plot(10*log10(analysis.diff_energy{1})) 
+% title('Diffuse: energy (dB)'), grid on
+% legend('sound-field', 'non-diffuse', 'diffuse')
+% %figure, plot(analysis.diff{1}(1,:)), title('diffuseness'), grid on, ylim([0 1])
 
 %%
 clear pars
@@ -143,7 +143,7 @@ pars.ls_dirs_deg = dirs_rad*180/pi;
 %% RENDER SH RIR TO TARGET LOUDSPEAKER LAYOUT
 % % Render
 tic, sirr_ls_rir = HOSIRR(sh_rir, pars); toc
-audiowrite(['HOSIRR_ls_o' num2str(demo_order) '.wav'], 0.9.*sirr_ls_rir, fs);
+%audiowrite(['HOSIRR_ls_o' num2str(demo_order) '.wav'], 0.9.*sirr_ls_rir, fs);
 % 
 % % Convolve the input stimulus with the rendered loudspeaker array RIR
 % ir_length = size(sirr_ls_rir, 1);
@@ -152,14 +152,14 @@ audiowrite(['HOSIRR_ls_o' num2str(demo_order) '.wav'], 0.9.*sirr_ls_rir, fs);
 % output = 0.99.*output./max(abs(output(:))); % normalise
 % audiowrite(['BFormatDemo_HOSIRR_o' num2str(demo_order) '.wav'], output, fs, 'BitsPerSample', 24);
 
-%% WIP: BINAURAL
-disp(' * BINAURAL')
+%% WIP: BINAURAL by Reencoding
+disp(' * BINAURAL Reenc')
 % load HRIRs
 pars.hrtf_sofa_path = sofapath;
 
 assert(isfile(pars.hrtf_sofa_path))
-[sirr_bin, sir_ndiff, sir_diff, pars, analysis] = HOSIRR_bin(sh_rir, pars);
-audiowrite(['HOSIRR_o' num2str(demo_order) '_bin.wav'], 0.9.*sirr_bin, fs);
+[sirr_binre, sir_ndiff, sir_diff, pars, analysis] = HOSIRR_bin_re(sh_rir, pars);
+%audiowrite(['HOSIRR_o' num2str(demo_order) '_binre.wav'], 0.9.*sirr_binre, fs);
 
 %hrir_0 = pars.hrirs(:, :, 6);
 %omni_bir = fftfilt(hrir_0, sqrt(4*pi)*sh_rir(:, 1));
@@ -167,7 +167,7 @@ LISTEN = true
 if LISTEN
 %sound(omni_bir, fs)
 %pause(2)
-sound(sirr_bin, fs)
+sound(sirr_binre, fs)
 pause(2)
 sound(sir_ndiff, fs)
 pause(2)
@@ -177,13 +177,13 @@ end
 
 
 %% WIP: BINAURAL Direct
-disp(' * BINAURAL')
+disp(' * BINAURAL Dir')
 % load HRIRs
 pars.hrtf_sofa_path = sofapath;
 
 assert(isfile(pars.hrtf_sofa_path))
 [sirr_bind, sir_ndiffd, sir_diffd, pars, analysis] = HOSIRR_bin_direct(sh_rir, pars);
-audiowrite(['HOSIRR_o' num2str(demo_order) '_bin.wav'], 0.9.*sirr_bin, fs);
+%audiowrite(['HOSIRR_o' num2str(demo_order) '_bind.wav'], 0.9.*sirr_binre, fs);
 
 %hrir_0 = pars.hrirs(:, :, 6);
 %omni_bir = fftfilt(hrir_0, sqrt(4*pi)*sh_rir(:, 1));
@@ -219,31 +219,36 @@ ls_proj = [x_hrfts, y_hrtfs, z_hrtfs] * [x_ls, y_ls, z_ls].';
 [d_min, d_min_k] = max(ls_proj);
 ls_sigs_bin_l = sum(fftfilt(squeeze(pars.hrirs(:, 1, d_min_k)), sirr_ls_rir), 2);
 ls_sigs_bin_r = sum(fftfilt(squeeze(pars.hrirs(:, 2, d_min_k)), sirr_ls_rir), 2);
-vls_sirr = [ls_sigs_bin_l, ls_sigs_bin_r];
+sirr_vls = [ls_sigs_bin_l, ls_sigs_bin_r];
 
 %% Compare
+if LISTEN
 disp("MagLS")
 sound(rir_magLS, fs)
 pause(2)
 disp("VLS HOSIRR")
-sound(vls_sirr, fs)
+sound(sirr_vls, fs)
 pause(2)
-disp("Binaural HOSIRR")
-sound(sirr_bin, fs)
+disp("Binaural Reenc HOSIRR")
+sound(sirr_binre, fs)
 pause(2)
-disp("Direct Binaural HOSIRR")
+disp("Binaural Direct HOSIRR")
 sound(sirr_bind, fs)
 pause(2)
-
-
+end
+audiowrite(['MagLS_o', num2str(demo_order), '.wav'], rir_magLS, fs);
+audiowrite(['HOSIRR_o', num2str(demo_order), '_sirr_vls.wav'], sirr_vls, fs);
+audiowrite(['HOSIRR_o', num2str(demo_order), '_sirr_binre.wav'], sirr_binre, fs);
+audiowrite(['HOSIRR_o', num2str(demo_order), '_sirr_bind.wav'], sirr_bind, fs);
 
 %out2 = matrixConvolver(sh_rir, permute(D_bin_filters,[3, 2, 1]), 2048);
 %soundsc(out2, fs)
 
+% check tail rms, should be very close
 %rms(omni_bir(0.5*fs:fs, :))
 rms(rir_magLS(0.5*fs:fs, :))
-rms(vls_sirr(0.5*fs:fs, :))
-rms(sirr_bin(0.5*fs:fs, :))
+rms(sirr_vls(0.5*fs:fs, :))
+rms(sirr_binre(0.5*fs:fs, :))
 rms(sirr_bind(0.5*fs:fs, :))
 
 %%
@@ -254,7 +259,7 @@ ylim([-1, 1])
 grid on
 title('VLS HOSIRR')
 subplot(4,1, 2)
-plot(sirr_bin)
+plot(sirr_binre)
 ylim([-1, 1])
 grid on
 title('BIN SIRR')
